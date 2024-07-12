279e50d616abf6e787776cc20afad049f02d40d1
quetzl <lnalex2456@gmail.com>
2024-07-12 14:37:26 -0700

    initialized database

--- app.py
+++ app.py
@@ -5,14 +5,20 @@ from datetime import datetime
 
 # How every Flask app starts
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
 db = SQLAlchemy(app)
 
 
 # db Model (required)
 class Projects(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    thumbnail = db.Column(db.BLOB)
+    description = db.Column(db.String(), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
 app.register_blueprint(views, url_prefix="/")
