4ed759f491703bf30dc842602caadf466cbe5aca
quetzl <lnalex2456@gmail.com>
2024-07-12 13:38:04 -0700

    initialized db

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from views import views
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # How every Flask app starts
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+db = SQLAlchemy(app)
+
+
+# db Model (required)
+class Projects(db.Model):
+    pass
+
 
 app.register_blueprint(views, url_prefix="/")
 
