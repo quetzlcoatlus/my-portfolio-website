6d701b8c1c4945f0e4d9c75875de89291f1c8473
quetzl <lnalex2456@gmail.com>
2024-07-31 12:16:06 -0700

    merged views into app to fix circular dependency

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from views import views
+from flask import Flask, Blueprint, render_template, request, redirect
+
 from flask_sqlalchemy import SQLAlchemy  # converts python into SQL
 from datetime import datetime
 
@@ -21,7 +21,38 @@ class Projects(db.Model):
         return '<Name %r>' % self.name
 
 
+views = Blueprint(__name__, "views")
 app.register_blueprint(views, url_prefix="/")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/projects")
+def projects():
+    if request.method == "POST":
+        project_name = request.form["project-name"]
+        new_project = Projects(name=project_name)
+
+        # Push to database
+        try:
+            db.session.add(new_project)
+            db.session.commit()
+            return redirect("/projects")
+        except:
+            return "error occurred"
+
+    else:
+        projects_list = Projects.query.order_by(Projects.date)
+        return render_template("projects.html", projects=projects_list)
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
